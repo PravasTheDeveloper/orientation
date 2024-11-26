d58c1a85a9c91b2a40d72d0ee2d1d9742c8aa365
PravasTheDeveloper <info.pravas.cs@gmail.com>
2024-11-27 00:40:52 +0600

    new update

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import HomePagePenel from '@/components/HomePage/HomePagePenel'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <HomePagePenel />
+    </>
+  )
+}
