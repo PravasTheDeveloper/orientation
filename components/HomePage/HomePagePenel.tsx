6b07b602de48299e5d27202815c846db33c53d42
PravasTheDeveloper <info.pravas.cs@gmail.com>
2024-11-27 02:41:07 +0600

    done2

--- components/HomePage/HomePagePenel.tsx
+++ components/HomePage/HomePagePenel.tsx
@@ -1,22 +1,11 @@
 import Image from "next/image";
 
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+
 
 import {
   Carousel,
